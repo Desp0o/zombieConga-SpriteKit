d9ededdc5b8a230ae0fe17dcf912f0396a602808
desp0o <tornike.despotashvili@gmail.com>
2025-07-02 14:47:09 +0400

    Initial Commit

---
+++ Zombie Conga/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Zombie Conga
+//
+//  Created by Tornike Despotashvili on 7/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Zombie Conga/Zombie_CongaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Zombie_CongaApp.swift
+//  Zombie Conga
+//
+//  Created by Tornike Despotashvili on 7/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Zombie_CongaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
